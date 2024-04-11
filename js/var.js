17f2741dc5a2d2396b78dc39ed67f1d6eeb2f8fb
andire120 <164429441+andire120@users.noreply.github.com>
2024-04-11 19:03:13 +0900

    자바 기초 let

---
+++ js/var.js
@@ -0,0 +1,29 @@
+let name = "이윤솔";
+let age = 17;
+let height = 160;
+let gf = false;
+let bf = undefined;
+let baby = null;
+
+console.log(name);
+console.log(age);
+console.log(height);
+console.log(gf);
+console.log(bf);
+console.log(baby);
+
+//제 이름은 name 이고, 17살에 여자친구는 false 남자친구는 undefined, 애기는 null입니다.
+
+console.log(
+  "제 이름은",
+  name,
+  "이고,",
+  age,
+  "살에 여자 친구는",
+  gf,
+  "남자친구는",
+  bf,
+  ",애기는 ",
+  baby,
+  "입니다."
+);
